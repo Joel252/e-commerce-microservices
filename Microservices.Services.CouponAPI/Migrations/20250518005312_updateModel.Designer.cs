26a7e9abef56aecedd6506019a30ef39d35ea452
Joel252 <joel.enrique.cruz860@gmail.com>
2025-05-18 01:02:42 -0500

    feat: add new migrations

--- Microservices.Services.CouponAPI/Migrations/20250518005312_updateModel.Designer.cs
+++ Microservices.Services.CouponAPI/Migrations/20250518005312_updateModel.Designer.cs
@@ -4,6 +4,7 @@ using Microservices.Services.CouponAPI.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Microservices.Services.CouponAPI.Migrations
 {
     [DbContext(typeof(CouponDbContext))]
-    partial class CouponDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250518005312_updateModel")]
+    partial class updateModel
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Microservices.Services.CouponAPI/Migrations/20250518005312_updateModel.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Microservices.Services.CouponAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class updateModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Coupons",
+                keyColumn: "CouponId",
+                keyValue: 1,
+                column: "LastUpdated",
+                value: new DateTime(2025, 3, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.UpdateData(
+                table: "Coupons",
+                keyColumn: "CouponId",
+                keyValue: 2,
+                column: "LastUpdated",
+                value: new DateTime(2025, 3, 2, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Coupons",
+                keyColumn: "CouponId",
+                keyValue: 1,
+                column: "LastUpdated",
+                value: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.UpdateData(
+                table: "Coupons",
+                keyColumn: "CouponId",
+                keyValue: 2,
+                column: "LastUpdated",
+                value: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+    }
+}
