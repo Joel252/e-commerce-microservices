e19a94d5827b3fb9c16f8c8deaaf2b7894c2f954
Joel252 <joel.enrique.cruz860@gmail.com>
2025-05-22 12:54:34 -0600

    feat: implement Identity authentication
    - Add Identity package
    - Add Name field to AspNetUsers
    - Add DbContext
    - Setup services in Program.cs

---
+++ Microservices.Services.AuthAPI/Data/AuthDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microservices.Services.AuthAPI.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Microservices.Services.AuthAPI.Data
+{
+    public class AuthDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+
+        public AuthDbContext(DbContextOptions<AuthDbContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
--- Microservices.Services.AuthAPI/Program.cs
+++ Microservices.Services.AuthAPI/Program.cs
@@ -1,25 +1,47 @@
+using Microservices.Services.AuthAPI;
+using Microservices.Services.AuthAPI.Data;
+using Microservices.Services.AuthAPI.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Add database context
+builder.Services.AddDbContext<AuthDbContext>(option =>
+{
+    option.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnectionString"));
+});
+
+// Add Identity service
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
+    .AddEntityFrameworkStores<AuthDbContext>()
+    .AddDefaultTokenProviders();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    // Enable Swagger UI in development environment
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
 
+// Enable authentication and authorization middleware
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
+
+// Apply database migrations
+app.ApplyMigration();
 
 app.Run();
