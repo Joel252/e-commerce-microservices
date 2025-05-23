531b0b4ba67a588f78355cfecb67919d3d9cd2dc
Joel252 <joel.enrique.cruz860@gmail.com>
2025-05-23 16:33:05 -0600

    refactor: add token life time property

--- Microservices.Services.AuthAPI/Models/JwtOptions.cs
+++ Microservices.Services.AuthAPI/Models/JwtOptions.cs
@@ -5,5 +5,7 @@
         public string? Issuer { get; set; }
         public string? Audience { get; set; }
         public string? SecretKey { get; set; }
+        public TimeSpan TokenLifetime { get; set; } = TimeSpan.FromHours(1);
+
     }
 }
