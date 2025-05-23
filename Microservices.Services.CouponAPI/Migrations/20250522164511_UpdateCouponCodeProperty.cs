32249c9579eff8502717d590319156e1c700eecd
Joel252 <joel.enrique.cruz860@gmail.com>
2025-05-22 21:50:50 -0600

    feat: add new migrations

---
+++ Microservices.Services.CouponAPI/Migrations/20250522164511_UpdateCouponCodeProperty.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Microservices.Services.CouponAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateCouponCodeProperty : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
