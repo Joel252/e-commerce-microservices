e954c532ddc82dab47aa1cbf13c6333d18f70073
Joel252 <joel.enrique.cruz860@gmail.com>
2025-05-23 13:20:39 -0600

    feat: define authentication service methods
    - define register method
    - define login method

--- Microservices.Services.AuthAPI/Service/IService/IAuthService.cs
+++ Microservices.Services.AuthAPI/Service/IService/IAuthService.cs
@@ -2,9 +2,22 @@
 
 namespace Microservices.Services.AuthAPI.Service.IService
 {
+    /// <summary>
+    /// Interface for authentication-related services.
+    /// </summary>
     public interface IAuthService
     {
-        Task<ApplicationUserDto> Register(RegisterRequestDto registerRequestDto);
+        /// <summary>
+        /// Registers a new user with the provided registration details.
+        /// </summary>
+        /// <param name="registerRequestDto">An object containing the user's registration details.</param>
+        /// <returns>A string containing a message</returns>
+        Task<string> Register(RegisterRequestDto registerRequestDto);
+        /// <summary>
+        /// Login a user with the provided credentials.
+        /// </summary>
+        /// <param name="loginRequestDto">An object containing the data required to log in</param>
+        /// <returns>A <see cref="LoginResponseDto"/> object containing the response details </returns>
         Task<LoginResponseDto> Login(LoginRequestDto loginRequestDto);
     }
 }
