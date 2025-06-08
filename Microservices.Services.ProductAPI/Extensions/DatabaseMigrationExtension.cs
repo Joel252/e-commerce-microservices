23b86eaecea16c34d1266612068c8f6b209319f0
Joel252 <joel.enrique.cruz860@gmail.com>
2025-06-07 19:39:48 -0500

    feat: implement database extension to apply pending migrations

---
+++ Microservices.Services.ProductAPI/Extensions/DatabaseMigrationExtension.cs
@@ -0,0 +1,45 @@
+using Microservices.Services.ProductAPI.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace Microservices.Services.ProductAPI.Extensions;
+
+/// <summary>
+/// Extension method for applying database migrations.
+/// </summary>
+public static class DatabaseMigrationExtension
+{
+    /// <summary>
+    /// Applies database migrations.
+    /// </summary>
+    /// <param name="app">The web application instance</param>
+    public static void ApplyMigration(this WebApplication app)
+    {
+        using var scope = app.Services.CreateScope();
+
+        // Get the database context from the service provider
+        var db = scope.ServiceProvider.GetRequiredService<ProductDbContext>();
+        // Get the logger from the service provider
+        var logger = scope.ServiceProvider.GetRequiredService<ILogger<Program>>();
+
+        logger.LogInformation("Checking for pending migrations...");
+
+        // Check if there are any pending migrations
+        var pendingMigrations = db.Database.GetPendingMigrations().ToList();
+        if (pendingMigrations.Count == 0)
+        {
+            logger.LogInformation("No pending migrations found.");
+            return;
+        }
+
+        try
+        {
+            logger.LogInformation("Found {count} pending migrations. Applying...", pendingMigrations.Count);
+            db.Database.Migrate(); // Apply migrations
+            logger.LogInformation("Migrations applied successfully.");
+        }
+        catch (Exception ex)
+        {
+            logger.LogError(ex, "An error occurred while applying migrations.");
+        }
+    }
+}
