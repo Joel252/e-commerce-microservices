efd50fe6688db02f63ff8b8759afa260f00b905f
Joel252 <joel.enrique.cruz860@gmail.com>
2025-06-08 23:42:06 -0500

    feat: setup mapper

---
+++ Microservices.Services.ShoppingCartAPI/MappingConfig.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using Microservices.Services.ShoppingCartAPI.Models;
+using Microservices.Services.ShoppingCartAPI.Models.DTO;
+
+namespace Microservices.Services.ShoppingCartAPI
+{
+    /// <summary>
+    /// Static class for configuring AutoMapper mappings.
+    /// </summary>
+    public static class MappingConfig
+    {
+        /// <summary>
+        ///  Registers the AutoMapper mappings.
+        /// </summary>
+        /// <returns>AutoMapper configuration.</returns>
+        public static MapperConfiguration RegisterMaps()
+        {
+            var mappingConfig = new MapperConfiguration(config =>
+            {
+                config.CreateMap<CartHeaderDto, CartHeader>();
+                config.CreateMap<CartHeader, CartHeaderDto>();
+                
+                config.CreateMap<CartDetailsDto, CartDetails>();
+                config.CreateMap<CartDetails, CartDetailsDto>();
+            });
+
+            return mappingConfig;
+        }
+    }
+}
